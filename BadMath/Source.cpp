7b189d217495f3073af729e5eeb87786ef5d7b33
Asgaard <Asgaard@DESKTOP>
2021-07-08 16:13:30 -0400

    The original average method was corrected in the source code for math formula and logical input errors.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,18 +2,24 @@
 #include <iomanip>
 using namespace std;
 
+
+//average method corrected for math formula and logical input errors.  
+
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (float)(i1 + i2 + i3) / 3.0f; //corrected logical math error to take correct mean 
+                                         //additionally cast numerator as float to make implict 
+	                                     //type conversion of numerator so quotient will be an
+	                                     //exact float for float average variable 
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1 = 0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3; //corrected logical error for n3 instead of n2
 
 	float a = average(n1, n2, n3);
 
